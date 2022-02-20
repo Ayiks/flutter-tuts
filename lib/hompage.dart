2ed3b1dfb293725af692a139ed3b8c411f372433
Ayiks <cliffysarp@gmail.com>
2022-02-20 11:11:35 +0000

    Added drawer(which contains the side menu)

--- lib/hompage.dart
+++ lib/hompage.dart
@@ -1,12 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:tuts/favourites_page.dart';
+import 'package:tuts/user_page.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('MY APP'),),
+      appBar: AppBar(
+        title: const Text('MY APP'),
+      ),
+      drawer: NavigationDrawer(),
     );
   }
+
+  // NavigationDrawer() => const NavigationDrawer();
+}
+
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildHeader(context),
+              buildMenuItems(context),
+            ],
+          ),
+        ),
+      );
+
+  Widget buildHeader(context) => Material(
+        color: Colors.blue.shade700,
+        child: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const UserPage()));
+          },
+          child: Container(
+            padding: EdgeInsets.only(
+                top: 24 + MediaQuery.of(context).padding.top, bottom: 24),
+            child: Column(
+              children: const [
+                CircleAvatar(
+                  radius: 52,
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1645034647873-b8b84007c2f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  'Ayiks Junior',
+                  style: TextStyle(fontSize: 28, color: Colors.white),
+                ),
+                Text(
+                  'junior@ayiks.com',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+  Widget buildMenuItems(context) => Wrap(
+        runSpacing: 16,
+        children: [
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            onTap: () {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => const HomePage()));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.favorite_border),
+            title: const Text('Favourites'),
+            onTap: () {
+              //close navigation drawer before
+              Navigator.pop(context);
+
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const FavouritesPage()));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.workspaces_outline),
+            title: const Text('Workflow'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.update),
+            title: const Text('Updates'),
+            onTap: () {},
+          ),
+          const Divider(
+            color: Colors.black54,
+          ),
+          ListTile(
+            leading: const Icon(Icons.account_tree_outlined),
+            title: const Text('Plugins'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications_outlined),
+            title: const Text('Notifications'),
+            onTap: () {},
+          )
+        ],
+      );
 }
